cdcdb5fb2f945d6f73aa70d0f7458152e6b65ca1
MonaEis <mona.eismann@gmx.de>
2025-03-26 15:31:46 +0100

    Code Cleaning

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,7 @@ function calculateCalories(event: Event){
         event.preventDefault();
         console.log("Juhu", genderInput.value)
     console.log("i've been clicked")
-    // const female = genderInput.value === "female";
-    // const male = genderInput.value === "male";
+
 
     //* Harris-Benedict-Formel
     if(genderInput.checked === true){
@@ -50,12 +49,3 @@ function calculateCalories(event: Event){
         outputEnergyKj.innerText = (Number(femaleTotal) * 4.184).toFixed(2);
     }
 }
-
-
-
-// - Zuerst musst du den Grundumsatz berechnen. Der Grundumsatz wird mithilfe der Harris-Benedict-Formel berechnet.
-//     - Grundumsatz bei Männern (Kalorien je Tag):
-//         - 66.47 + (13.7 * Körpergewicht in kg) + (5 * Körpergröße in cm) – (6.8 * Alter in Jahren) = Grundumsatz
-//     - Grundumsatz bei Frauen (Kalorien je Tag):
-//         - 655.1 + (9.6 * Körpergewicht in kg) + (1.8 * Körpergröße in cm) – (4.7 * Alter in Jahren) = Grundumsatz
-// - Um den Gesamtumsatz auszurechnen, sollte der PAL-Faktor (PAL=Physical Activity Level) mit dem Grundumsatz multipliziert werden.
